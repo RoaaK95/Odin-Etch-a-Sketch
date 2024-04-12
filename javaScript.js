321a1f6b439b114ed266dd631c0b065762595c37
Roaa K <ruaakhuldoon.26@gmail.com>
2024-04-12 12:49:13 +0300

    MAKE 16*16 GRID

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,12 @@
+const container=document.getElementById("container");
 
+function createGrid(numDiv)
+{
+    for(let i=0;i<numDiv;i++)
+    {
+      const div=document.createElement("div");
+      container.appendChild(div);
+    }
+}
+
+createGrid(16*16);
